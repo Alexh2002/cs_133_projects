be8afeff80dc8b3e5c0faa12470a5e7194566a32
Alexander <a13x.2002@gmail.com>
2021-01-18 13:04:49 -0700

    This is my multiplication table

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myMultiDimensionalArray = new int[10, 10];
+
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    myMultiDimensionalArray[x, y] = (x + 1) * (y + 1);
+                }
+            }
+
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    Console.Write(myMultiDimensionalArray[x, y] + " " );
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
